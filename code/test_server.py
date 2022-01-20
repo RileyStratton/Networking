e3c9a0e776986b7dcab4a9793b5e4f8cdcfa5152
RileyStratton <riley.s.stratton@outlook.com>
2022-01-19 20:59:32 -0700

    server now receives infinite messages

--- code/test_server.py
+++ code/test_server.py
@@ -8,18 +8,17 @@ with open("code/ip.json") as infile:
 HOST = ip["desktop"]["ip"]                 # Symbolic name meaning all available interfaces
 PORT = ip["desktop"]["port"]               # Arbitrary non-privileged port
 
-print(HOST + ":" + PORT)
+print(HOST + ":" + str(PORT))
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
 
     print(f"Server started on {HOST}:{PORT}")
-    print(f"Waiting for clients...")
 
     s.listen(1)
     conn, addr = s.accept()
     with conn:
-        print('Connected by', addr)
+        print(f"Client connected from {addr[0]}:{addr[1]}")
         while True:
             msg = conn.recv(1024).decode()
             if msg != "":
